5e530933b79106da1e7658c155a9ed4dd8950c03
Vinicius Simas <viniciussimaslima19@gmail.com>
2024-10-30 17:20:20 -0300

    primeiro-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import JobCarousel from './components/JobCarousel';
 import './App.css';
+import logo from './assets/logo.png';
+
+const jobs = [
+  {
+    title: 'Representante Comercial',
+    description: 'Descrição: Desejável experiência com atendimento ao cliente...',
+    logo: 'https://yata-apix-a72da87a-7d6e-4d6a-b431-451af7303de8.s3-object.locaweb.com.br/a7a3506ea16c44d69698506c7901f10f.png',
+  },
+  {
+    title: 'Assistente de Suporte Comercial',
+    description: 'Descrição: Prestar suporte em controles gerenciais...',
+    logo: logo,
+  },
+  {
+    title: 'Analista de Suporte',
+    description: 'Descrição: Desejável experiência com atendimento ao cliente...',
+    logo: logo,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+      <h2 className="abc2">RECIFE</h2>
+        <h2 className="abc">290 vagas encontradas</h2> <hr className='linha'></hr>
+        <JobCarousel jobs={jobs} />
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>RE@AÇÃO 2024. Todos os direitos reservados. v5.61.0</p>
+    <div className="footer-icons">
+      {/* Aqui você pode adicionar ícones de redes sociais */}
+    </div>
+  </footer>
+);
+
+export default Footer;
